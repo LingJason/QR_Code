1b157cf955880016553e1ef2c786fc6da5642d50
Jason Ling <Jasonling1996@gmail.com>
2024-07-17 18:13:01 -0700

    clear files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Home from './components/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+
+  return (
+      <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
+      </div>
+  )
+}
+
+export default App
