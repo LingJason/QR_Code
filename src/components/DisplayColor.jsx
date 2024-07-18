ad660da8297eeb7068ca179591f546d273859f14
Jason Ling <Jasonling1996@gmail.com>
2024-07-17 18:25:40 -0700

    Add logic and css into display

---
+++ src/components/DisplayColor.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./DisplayColor.css";
+
+export default function DisplayColor({ color }) {
+  <div className="display-color" style={{ backgroundColor: color }}></div>;
+}
