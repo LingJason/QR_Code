f8b9cd0a8dc7238800b9b54f08c2e93f941a09b0
Jason Ling <Jasonling1996@gmail.com>
2024-07-17 18:31:22 -0700

    Add QRCard logic and css

---
+++ src/components/QRCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { handleDownload } from "./handlers";
+import "./QRCard.css";
+
+export default function QRCode ({ qrcodes }) {
+
+  return (
+    <div className="qr-code-section">
+      {qrcodes.map((svg, index) => (
+        <QRCodeItem key={index} svg={svg} index={index} />
+      ))}
+    </div>
+  );
+};
+
+const QRCodeItem = ({ svg, index }) => (
+  <div className="qr-code mb-3">
+    <div dangerouslySetInnerHTML={{ __html: svg }} />
+    <button
+      className="btn btn-success mt-3 download-link"
+      onClick={() => handleDownload(svg, index)}
+    >
+      Download {index > 0 ? `QR Code ${index}` : "QR Code"}
+    </button>
+  </div>
+);
