798f798f7367412d4516f8d9e7c9abb2bc9ccb0e
Jason Ling <Jasonling1996@gmail.com>
2024-07-17 18:14:57 -0700

    Add Navbar and css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/Navbar";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+          <span className="navbar-name">QRify</span>
+          <img
+            src="https://www.svgrepo.com/show/308831/qr-code-scanner-phone-qr-code-smartphone.svg"
+            alt="logo"
+            className="navbar-logo"
+          />
+      </div>
+    </nav>
+  );
+}
